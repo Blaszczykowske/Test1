281753a94d78de4aa793e2e2e77b47c0d48d5edb
unknown <dhimanjyotikalita@gmail.com>
2025-07-20 13:41:44 +0530

    initial commit

---
+++ JustChecking.py
@@ -0,0 +1,2 @@
+def function():
+    return ("This is a test script to check the Python environment.")
---
+++ test_justchecking.py
@@ -0,0 +1,5 @@
+from JustChecking import function
+
+def test_just_checking():
+    function()
+    assert function() == "This is a test script to check the Python environment."
